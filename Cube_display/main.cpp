cb3cb2aad7a22477242a9e5c6a88b627f2e2d668
Han Zhang <pacrocodile@gmail.com>
2015-11-30 14:21:21 -0600

    Minor update

--- Cube_display/main.cpp
+++ Cube_display/main.cpp
@@ -8,10 +8,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    //Dialog k;
-    //k.show();
+   // MainWindow w;
+   // w.show();
+    Dialog k;
+    k.show();
 
     return a.exec();
 }
